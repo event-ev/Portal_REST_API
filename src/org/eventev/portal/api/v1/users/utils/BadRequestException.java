974423f64ec311721fe5d6ff3ee896c5f858d062
Simon Kurka <dev@simon.kurka.cc>
2014-04-27 12:41:33 +0200

    First Code checkin some Exception handling

---
+++ src/org/eventev/portal/api/v1/users/utils/BadRequestException.java
@@ -0,0 +1,18 @@
+package org.eventev.portal.api.v1.users.utils;
+
+public class BadRequestException extends Exception {
+
+	private static final long serialVersionUID = 6401517794053507579L;
+	
+	private int errorCode;
+	
+	public BadRequestException(int errorCode, String message) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+}
